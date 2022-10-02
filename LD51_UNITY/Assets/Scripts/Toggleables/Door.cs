1ee3654126c49698c07e2d14749c46d00b83155f
Kleissen,H.D.J. (Hilko) <h.d.j.kleissen@students.uu.nl>
2022-10-02 06:20:43 +0200

    Tiny door fix

--- LD51_UNITY/Assets/Scripts/Toggleables/Door.cs
+++ LD51_UNITY/Assets/Scripts/Toggleables/Door.cs
@@ -8,11 +8,16 @@ public class Door : Toggleable
     [SerializeField] Animator DoorAnimator;
     [SerializeField] Collider2D DoorCollider;
     [SerializeField] AnimationClip DoorOpenClip, DoorCloseClip;
+    [SerializeField] Sprite OpenDoorSprite;
+
+    private void Update()
+    {
+        spriteRenderer.sortingOrder = spriteRenderer.sprite == OpenDoorSprite ? 0 : 1;
+    }
 
     public override void HandleStateChange()
     {
         // Needs to be on ground "layer" when open
-        spriteRenderer.sortingOrder = IsToggledOn ? 1 : 0;
         DoorCollider.enabled = IsToggledOn;
         DoorAnimator.Play(IsToggledOn ? DoorCloseClip.GetClipName() : DoorOpenClip.GetClipName());
     }
